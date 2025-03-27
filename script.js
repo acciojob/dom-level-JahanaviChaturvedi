27c5b96ff2aa4ffe07f142fc1f42ad44abd33a29
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-27 21:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+function getDOMLevel(element) {
+    let level = 0;
+    let currentElement = element;
+
+    while (currentElement) {
+        level++;
+        currentElement = currentElement.parentElement; // Move to the parent element
+    }
+
+    return level;
+}
+
+const targetElement = document.getElementById('level');
+
+const domLevel = getDOMLevel(targetElement);
+alert
